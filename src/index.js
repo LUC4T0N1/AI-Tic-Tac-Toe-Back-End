c5f6dc7ca537b51a5b2215accd086b16251fa5de
LUC4T0N1 <lucas_moniz@hotmail.com>
2022-07-21 18:23:42 -0300

    io.listen

--- src/index.js
+++ src/index.js
@@ -72,7 +72,6 @@ io.on("connection", (socket) => {
 
 });
 
-
-server.listen(process.env.PORT || 8080, () => {
+io.listen(process.env.PORT || 8080, () => {
   console.log('listening on port *' + process.env.PORT );
 });
